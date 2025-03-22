3800f0c5bd6ba2f38a448e841c860125e2b59125
csnpcjexpress <84871549+csnpch@users.noreply.github.com>
2025-03-22 22:49:08 +0700

    fix copy

--- POC/3.การ Create script ออกมา (และรองรับการทดสอบแบบหลาย Type)/generator/utils/helper.ts
+++ POC/3.การ Create script ออกมา (และรองรับการทดสอบแบบหลาย Type)/generator/utils/helper.ts
@@ -4,6 +4,10 @@ export const f = (s: string) => s?.split('\n')?.map((x) => x.replace('  ', ''))?
 
 export const copyFolder = async (source_dir: string, destination_dir: string) => {
   try {
+    // Remove the destination directory if it exists
+    if (await fs.pathExists(destination_dir)) {
+      await fs.remove(destination_dir);
+    }
     await fs.copy(source_dir, destination_dir)
   } catch (err) {
     console.error('Error copying folder:', err);
