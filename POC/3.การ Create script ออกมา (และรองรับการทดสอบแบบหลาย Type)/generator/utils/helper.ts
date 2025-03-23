54e95d7ebf13383a4edf32280e56cff67d5626f0
csnpcjexpress <84871549+csnpch@users.noreply.github.com>
2025-03-24 01:17:05 +0700

    checkpoint

--- POC/3.การ Create script ออกมา (และรองรับการทดสอบแบบหลาย Type)/generator/utils/helper.ts
+++ POC/3.การ Create script ออกมา (และรองรับการทดสอบแบบหลาย Type)/generator/utils/helper.ts
@@ -1,5 +1,8 @@
 import fs from 'fs-extra'
 import beautify from 'js-beautify';
+import { gzip } from 'zlib';
+import { promisify } from 'util';
+const gzipPromise = promisify(gzip);
 
 export const f = (s: string) => beautify.js(s, { indent_size: 2, preserve_newlines: false })?.toString()?.toString()?.replace(/export/g, '\nexport')
 
@@ -12,5 +15,16 @@ export const copyFolder = async (source_dir: string, destination_dir: string) =>
     await fs.copy(source_dir, destination_dir)
   } catch (err) {
     console.error('Error copying folder:', err);
+  }
+}
+
+export const compressAndSaveToGzip = async (data: any, outputPath: string) => {
+  try {
+    const gzipBuffer = await gzipPromise(typeof data === 'string' ? data : JSON.stringify(data));
+    const outputFile = fs.createWriteStream(outputPath);
+    outputFile.write(gzipBuffer);
+    console.info(`[INFO]: Created ${outputPath} successfully`);
+  } catch (error) {
+    console.error(`[ERROR]: Failed to create ${outputPath}:`, error);
   }
 }
